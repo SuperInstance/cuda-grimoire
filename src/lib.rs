4241b1104074c5b3af1edaabc991d28b215bbc97
Lucineer <magnus.digennaro@gmail.com>
2026-04-11 22:02:54 -0800

    add: src/lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod compendium;
+pub mod pattern;
+pub mod recipe;
+pub mod spellbook;
+
+pub use compendium::FleetCompendium;
+pub use pattern::{AgentPattern, PatternCategory};
+pub use recipe::built_in_recipes;
+pub use spellbook::Spellbook;
